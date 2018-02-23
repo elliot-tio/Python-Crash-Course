4f93d4fa3186fa7ab05f5f4b73cbff48d0d919c0
Elliot Tio <etjinzo@gmail.com>
2018-02-23 17:28:42 -0500

    Continued Ch 3

---
+++ Ch. 3/bicycles.py
@@ -0,0 +1,12 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+# prints first thing on the list
+print("First: " + bicycles[0])
+
+# prints last thing on the list
+print("Last: " + bicycles[-1])
+
+# uses list value to compose a message
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
---
+++ Ch. 3/names_greetings_and_lists.py
@@ -0,0 +1,19 @@
+# Names
+print("Names:")
+names = ['Elliot', 'Patty', 'Steffi']
+for name in names:
+    print(name)
+print(end='\n')
+
+# Greetings
+print("Greeetings:")
+print("Hello, " + names[0] + "!")
+print("Hello, " + names[1] + "!")
+print("Hello, " + names[2] + "!")
+print(end='\n')
+
+# Own List
+print("Own list:")
+cars = ['Audi', 'Honda', 'Tesla']
+print(cars)
+print("I want an " + cars[0] + ".")
